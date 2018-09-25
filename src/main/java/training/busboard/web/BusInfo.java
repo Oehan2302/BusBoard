158a2951fb055681157d942228318f6dfe2ee16b
Rupert Foggo McKay <rupert.mckay@softwire.com>
2018-09-25 14:29:49 +0100

    adds skeleton web framework=

---
+++ src/main/java/training/busboard/web/BusInfo.java
@@ -0,0 +1,14 @@
+package training.busboard.web;
+
+ public class BusInfo {
+    private final String postcode;
+    
+     public BusInfo(String postcode) {
+        this.postcode = postcode;
+    }
+     
+     public String getPostcode() {
+        return postcode;
+    }
+}
+ 
